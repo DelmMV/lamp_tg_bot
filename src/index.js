a30b9ef15e4c7a6097ddbf8e92eaf9cb604bb937
DelmMV <opi588@yandex.ru>
2023-11-20 17:24:18 +0300

    add /alarm

--- src/index.js
+++ src/index.js
@@ -21,6 +21,34 @@ function sendMessageUser(chatId, message) {
   });
 }
 
+bot.command('alarm', async (ctx) => {
+  // Получаем текст после команды /alarm
+  const text = ctx.message.text.split(' ').slice(1).join(' ');
+
+  // Если нет текста, уведомляем пользователя
+  if (!text || !text.includes(',')) {
+    return ctx.reply('Пожалуйста, введите через запятую текст с описанием и сылку на сообщения после команды /alarm.');
+  }
+
+  // Разделяем текст на ссылку и описание поста
+  const [description,link] = text.split(',');
+
+  // Убираем лишние пробелы
+  const postDescription = description.trim();
+  const postLink = link.trim();
+
+  // Отправляем информацию о посте и описании в административный чат
+  const message = `
+  Новый тревожный сигнал от пользователя ${ctx.message.from.first_name} ${ctx.message.from.last_name ? ctx.message.from.last_name : ''}:
+  Ссылка на пост: ${postLink}
+  Описание: ${postDescription}
+  `;
+
+  await sendMessageAdminChat(-1001295808191, message, { message_thread_id: 17137, parse_mode: 'HTML' }); // Замените ID чата на свой
+
+  ctx.reply('Ваш тревожный сигнал отправлен администратору.');
+});
+
 bot.on('new_chat_members', async (ctx) => {
   const replyRequest = `
   <a href="tg://user?id=${ctx.message.from.id}">${ctx.message.from.first_name} ${ctx.message.from.last_name ? ctx.message.from.last_name : ""}</a> принят(а) в группу
@@ -42,9 +70,8 @@ bot.on('chat_join_request', async (ctx) => {
   Язык юзера: ${ctx.chatJoinRequest.from.language_code}
   `;
   const answer = `
-  Здравствуйте! Вы направили заявку на вступление в сообщество МоноПитер ( t.me/eucriders ).
-  Такие заявки мы проверяем на ботов. Чтобы мы одобрили заявку, прошу написать в ответ, какое у вас моноколесо или прикрепить его фото. Если не будет ответа на это сообщение в течение суток, оставляем за собой право отклонить вашу заявку.
-  В ожидании одобрения, предлагаю ознакомиться с правилами/ценностями нашего сообщества: https://t.me/eucriders/287907/365863
+  Чтобы мы одобрили заявку, прошу написать в ответе, какое у вас моноколесо или прикрепить его фото. Если не будет ответа на это сообщение в течение суток, оставляем за собой право отклонить вашу заявку.
+  В ожидании одобрения, предлагаю ознакомиться с правилами/ценностями нашего сообщества: https://t.me/eucriders/287907/403321
   Спасибо за понимание!
   `;
   
